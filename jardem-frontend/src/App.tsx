6c1a7ef16fbce91bb2e8664feb451a93b2e34e80
leanderfitze <leanderfitze@gmail.com>
2023-05-21 22:06:59 +0200

    add frontend skeleton

---
+++ jardem-frontend/src/App.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+import { Header, Image, List } from 'semantic-ui-react'
+import { request } from 'http'
+
+function App() {
+  const [requests, setRequests] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/requests').then((response) => {
+      console.log(response)
+      setRequests(response.data)
+    })
+  }, [])
+
+  return (
+    <>
+      <Header as='h2'>
+        <Image centered src='./assets/logo.png' />
+        <Header.Content>Jardem</Header.Content>
+      </Header>
+      <List>{requests.map((request:any)=>(
+        <List.Item>{request.title}</List.Item>
+      ))}</List>
+    </>
+  )
+}
+
+export default App
